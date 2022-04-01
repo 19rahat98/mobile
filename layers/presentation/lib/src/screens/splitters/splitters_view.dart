1af3ecf7b6fcd1c8e07386467606fbcf6ac624df
Morgan Hunt <mrgnhnt96@gmail.com>
2022-04-01 08:22:06 -0500

    feat: add nav to create splitter

--- layers/presentation/lib/src/screens/splitters/splitters_view.dart
+++ layers/presentation/lib/src/screens/splitters/splitters_view.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:presentation/src/routes/paths.dart';
 
 class SplittersView extends StatelessWidget {
   const SplittersView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'this is the Splitters view.',
+    return Center(
+      child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              context.push(Paths.home.splitters.create.path);
+            },
+            child: const Text('Create Splitter'),
+          ),
+        ],
       ),
     );
   }
