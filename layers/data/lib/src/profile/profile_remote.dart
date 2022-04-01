ce9a79287b8bc714543b3cb665f573bc5fe3ecbf
Morgan Hunt <mrgnhnt96@gmail.com>
2022-03-31 23:24:40 -0500

    feat(wip): return default values

--- layers/data/lib/src/profile/profile_remote.dart
+++ layers/data/lib/src/profile/profile_remote.dart
@@ -1,32 +1,44 @@
-// ignore_for_file: avoid_print
-
 import 'package:data/src/profile/i_profile_repos.dart';
 import 'package:data/src/util/request_result.dart';
 import 'package:domain/domain.dart';
 
 class ProfileRemote extends IProfileRemote {
   @override
-  Future<RequestResult<Profile>> login(String email) {
-    throw UnimplementedError();
+  Future<RequestResult<PreProfile>> login(String email, String password) async {
+    return RequestResult.success(
+      Profile(
+        id: 'id',
+        email: email,
+        firstName: 'firstName',
+        lastName: 'lastName',
+        phoneNumber: const PhoneNumber(
+          phoneNumber: 'phoneNumber',
+          id: 'id',
+        ),
+      ),
+    );
   }
 
   @override
-  Future<RequestResult<void>> logout() {
-    throw UnimplementedError();
+  Future<RequestResult<void>> logout() async {
+    return const RequestResult.success(null);
   }
 
   @override
-  Future<RequestResult<PreProfile?>> profile(String id) {
-    throw UnimplementedError();
+  Future<RequestResult<PreProfile?>> profile(String id) async {
+    return const RequestResult.success(null);
   }
 
   @override
-  Future<RequestResult<Profile>> update(Profile profile) {
-    throw UnimplementedError();
+  Future<RequestResult<Profile>> update(Profile profile) async {
+    return RequestResult.success(profile);
   }
 
   @override
-  Future<RequestResult<PreProfile>> create(String email) {
-    throw UnimplementedError();
+  Future<RequestResult<PreProfile>> create(
+    String email,
+    String password,
+  ) async {
+    return RequestResult.success(PreProfile(email: email, id: 'id'));
   }
 }
